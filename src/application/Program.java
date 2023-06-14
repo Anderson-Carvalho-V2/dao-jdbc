b23f13d1ac8d8dc3d336c0ab7d9982262c21010d
Anderson <andersson_73@live.com>
2023-06-14 09:59:00 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Date;
 
 import db.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 
 public class Program {
@@ -13,6 +15,7 @@ public class Program {
 		Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, department);
 		System.out.println(seller);
 		
+		SellerDao sellerDao = DaoFactory.createSllerDao();
 
 	}
 
