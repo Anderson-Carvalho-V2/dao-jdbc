cd3ed0681712e390c9ce4c860bf4a4a96259d308
Anderson <andersson_73@live.com>
2023-06-14 01:26:16 -0300

    classe de entidade vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
+import db.Seller;
 import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department objs = new Department(1, "books");
-		System.out.println(objs);
+		Department department = new Department(1, "books");
+		Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, department);
+		System.out.println(seller);
 		
 
 	}
